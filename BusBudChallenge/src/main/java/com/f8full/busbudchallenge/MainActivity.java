1b2d6aaed1af09aed9f96419f7aff0761e1171ec
F8Full <f8full@gmail.com>
2014-01-27 11:25:31 -0500

    Added http request support on busbud API endpoint

--- BusBudChallenge/src/main/java/com/f8full/busbudchallenge/MainActivity.java
+++ BusBudChallenge/src/main/java/com/f8full/busbudchallenge/MainActivity.java
@@ -1,18 +1,38 @@
 package com.f8full.busbudchallenge;
 
+import android.os.AsyncTask;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends ActionBarActivity {
+
+
+    public static final String ENDPOINT = "https://api-staging.busbud.com/";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +44,8 @@ public class MainActivity extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+
+        new HttpAsyncTask().execute(ENDPOINT + "en/api/v1/languages");
     }
 
 
@@ -75,6 +97,61 @@ public class MainActivity extends ActionBarActivity {
         ft.add(R.id.container, frag);
         ft.addToBackStack(WebFragment.class.getSimpleName());
         ft.commit();
+    }
+
+
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            InputStream inputStream = null;
+            String result = "";
+            try {
+
+                // create HttpClient
+                HttpClient httpclient = new DefaultHttpClient();
+
+                // make GET request to the given URL
+                HttpGet getRequest = new HttpGet(urls[0]);
+
+                getRequest.addHeader("X-Busbud-Application-ID", BusbudCredentials.APP_ID);
+                getRequest.addHeader("X-Busbud-Device-Token", BusbudCredentials.DEVICE_TOKEN);
+
+                HttpResponse httpResponse = httpclient.execute(getRequest);
+
+                // receive response as inputStream
+                inputStream = httpResponse.getEntity().getContent();
+
+                // convert inputstream to string
+                if(inputStream != null)
+                    result = convertInputStreamToString(inputStream);
+                else
+                    result = "Did not work!";
+
+            } catch (Exception e) {
+                Log.d("InputStream", e.getLocalizedMessage());
+            }
+
+            return result;
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getBaseContext(), "Received!", Toast.LENGTH_LONG).show();
+            //etResponse.setText(result);
+        }
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
     }
 
 }
