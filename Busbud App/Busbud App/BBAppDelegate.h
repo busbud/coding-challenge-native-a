930c2bda05042760895cb232743ef365c441f180
Dan Greencorn <dan@dangreencorn.ca>
2014-03-01 12:26:52 -0500

    Initial commit
    — Added empty Xcode project.

---
+++ Busbud App/Busbud App/BBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BBAppDelegate.h
+//  Busbud App
+//
+//  Created by Dan Greencorn on 3/1/2014.
+//  Copyright (c) 2014 Dan Greencorn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
