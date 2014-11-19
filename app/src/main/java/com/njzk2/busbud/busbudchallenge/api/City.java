550b6ce7a027ad31e45139d7c160a9abb3092359
njzk2 <masperos+github@gmail.com>
2014-11-18 19:02:24 -0500

    add Retrofit and define the endpoints of the api

---
+++ app/src/main/java/com/njzk2/busbud/busbudchallenge/api/City.java
@@ -0,0 +1,14 @@
+package com.njzk2.busbud.busbudchallenge.api;
+
+import java.io.Serializable;
+
+/**
+ * Created by simon on 18/11/14.
+ */
+public class City implements Serializable {
+
+    public String cityId;
+    public String fullName;
+    public String cityUrl;
+
+}
---
+++ app/src/main/java/com/njzk2/busbud/busbudchallenge/api/Token.java
@@ -0,0 +1,9 @@
+package com.njzk2.busbud.busbudchallenge.api;
+
+/**
+ * Created by niluge on 18/11/14.
+ */
+public class Token {
+    public boolean success;
+    public String token;
+}
