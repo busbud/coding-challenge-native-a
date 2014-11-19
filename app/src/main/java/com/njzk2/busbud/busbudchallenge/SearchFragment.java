b61822930de03b5b66b7a2e67334667582e23a78
njzk2 <masperos+github@gmail.com>
2014-11-18 19:30:38 -0500

    Add basic autocomplete function

--- app/src/main/java/com/njzk2/busbud/busbudchallenge/SearchFragment.java
+++ app/src/main/java/com/njzk2/busbud/busbudchallenge/SearchFragment.java
@@ -1,11 +1,14 @@
 package com.njzk2.busbud.busbudchallenge;
 
 import android.app.Fragment;
-import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.njzk2.busbud.busbudchallenge.api.City;
 
@@ -13,6 +16,7 @@ import com.njzk2.busbud.busbudchallenge.api.City;
  * Created by simon on 18/11/14.
  */
 public class SearchFragment extends Fragment {
+    private City destination;
 
     public static SearchFragment getInstance(City city) {
         SearchFragment fragment = new SearchFragment();
@@ -26,6 +30,24 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_search, container, false);
+        final TextView fromView = (TextView) rootView.findViewById(R.id.from_tv);
+        final City origin = (City) getArguments().getSerializable("city");
+        fromView.setText(origin.fullName);
+
+        final Button search = ((Button) rootView.findViewById(R.id.search));
+        // Button is disabled until a valid city is selected
+        search.setEnabled(false);
+        final AutoCompleteTextView toView = (AutoCompleteTextView) rootView.findViewById(R.id.to_tv);
+        final CityAdapter adapter = new CityAdapter(getActivity(), origin);
+        toView.setAdapter(adapter);
+        toView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                destination = adapter.getCity(position);
+                search.setEnabled(true);
+            }
+        });
+
         return rootView;
     }
 }
