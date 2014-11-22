8669f50d248743854df8265cd3fc4cfc91dca65e
Jean-Baptiste Morin <jb.morin@gmail.com>
2014-11-22 13:14:12 -0500

    [Busbud] Constants and LocationHelper

---
+++ app/src/main/java/jbm/busbud/AppConstants.java
@@ -0,0 +1,12 @@
+package jbm.busbud;
+
+/**
+ *
+ * @author Jean-Baptiste Morin - jb.morin@gmail.com
+ */
+public class AppConstants {
+
+    public static final String SHARED_PREF_NAME = "jbm.busbud";
+
+    public static final String KEY_HAS_SEEN_HELP_ONCE = "jbm.busbud.hasseenhelponce";
+}
