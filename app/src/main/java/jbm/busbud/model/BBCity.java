f9b0989e68bcb1830e433dee45c7d0224ec3f556
Jean-Baptiste Morin <jb.morin@gmail.com>
2014-11-22 11:06:36 -0500

    [BusBud] Add a basic BBCity Model object

---
+++ app/src/main/java/jbm/busbud/model/BBCity.java
@@ -0,0 +1,66 @@
+package jbm.busbud.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * The basic BusBud City object.
+ * It's parcelable to be easily pass through Bundles.
+ * @author Jean-Baptiste Morin - jb.morin@gmail.com
+ */
+public class BBCity implements Parcelable {
+
+    private final String mCityId;
+    private final String mCityUrl;
+    private final String mFullName;
+
+    public BBCity(String id, String url, String fullName) {
+        mCityId = id;
+        mCityUrl = url;
+        mFullName = fullName;
+    }
+
+    private BBCity(Parcel in) {
+        mCityId = in.readString();
+        mCityUrl = in.readString();
+        mFullName = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(mCityId);
+        dest.writeString(mCityUrl);
+        dest.writeString(mFullName);
+    }
+
+    public static final Parcelable.Creator<BBCity> CREATOR = new Parcelable.Creator<BBCity>() {
+        public BBCity createFromParcel(Parcel in) {
+            return new BBCity(in);
+        }
+
+        public BBCity[] newArray(int size) {
+            return new BBCity[size];
+        }
+    };
+
+    /**
+     * Getters
+     */
+
+    public String getCityId() {
+        return mCityId;
+    }
+
+    public String getCityUrl() {
+        return mCityUrl;
+    }
+
+    public String getFullName() {
+        return mFullName;
+    }
+}
