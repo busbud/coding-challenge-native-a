befc4861110c1e0555581f8c8fa428d56ca65d62
Romain Pouclet <palleas@gmail.com>
2014-11-27 16:39:53 -0500

    Bootstrap application

---
+++ Busbud/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Busbud
+//
+//  Created by Romain Pouclet on 2014-11-27.
+//  Copyright (c) 2014 Romain Pouclet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Busbud/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Busbud
+//
+//  Created by Romain Pouclet on 2014-11-27.
+//  Copyright (c) 2014 Romain Pouclet. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
