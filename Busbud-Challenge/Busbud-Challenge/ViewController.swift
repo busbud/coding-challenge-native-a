ea66f0e6514470bd983b8749ac91b40b6459c922
saadjulien <julien.saad@polymtl.ca>
2015-03-02 18:05:45 -0500

    Initial iOS setup with required Pods

---
+++ Busbud-Challenge/Busbud-Challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Busbud-Challenge
+//
+//  Created by Julien Saad on 2015-03-02.
+//  Copyright (c) 2015 Julien Saad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
